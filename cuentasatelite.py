99f820ea8f3fae67c277bac6d1112c68a40624c1
Rubén Galicia <46079748+JRubenGaliciaB@users.noreply.github.com>
2025-01-26 10:37:09 -0600

    Update cuentasatelite.py

--- cuentasatelite.py
+++ cuentasatelite.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 # Configuración inicial del título
 st.set_page_config(page_title="Cuesta Satélite de Cultura SHCP", layout="wide")
-st.title("Cuenta Satélite de Cultura SHCP")
+st.title("Cuenta Satélite de Cultura SHCP - Entidades Federativas")
 
 # Carga del archivo CSV desde la misma ubicación del programa
 csv_file = "conjunto_de_datos_cscm_csc_efacr_s2023_p.csv"
